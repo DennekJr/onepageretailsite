a856083cf977e8cadadea9467378de49faae0bf2
DennekJr <54910517+DennekJr@users.noreply.github.com>
2024-11-09 22:54:17 +0100

    Update utils.ts

--- src/app/(home)/utils.ts
+++ src/app/(home)/utils.ts
@@ -5,7 +5,7 @@ export type ComponentDataBAseType = {
   updatedAt: string;
   publishedAt: string;
 };
-
+//setup hosted strapi backend
 export const getComponentData = (pageComponent: string) => {
   return fetch(`https://newworld-solutions-strapi-git-main-chidumebi-ohiris-projects.vercel.app/api/${pageComponent}`).then((res) =>
     res.json(),
